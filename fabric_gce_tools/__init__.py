fc7ca39f8962ecc8a9f997326712162955bd474f
Eran Sandler <eran@sandler.co.il>
2015-03-29 20:49:56 +0300

    moved to use gcloud instead of gcutil because gcutil will be deprecated soon

--- fabric_gce_tools/__init__.py
+++ fabric_gce_tools/__init__.py
@@ -29,16 +29,36 @@ except ImportError:
 
 from fabric.api import env
 
-_gcutil_exists = None
+_gcloud_exists = None
 
-def _check_gcutil():
-    gcutil_version = None
+def _check_gcloud():
+    gcloud_version = None
     try:
-        gcutil_version = subprocess.check_output("gcutil version", shell=True)
+        gcloud_version = subprocess.check_output("gcloud version", shell=True)
     except subprocess.CalledProcessError:
-        raise Exception("Failed to run 'gcutil version'. That means you don't have gcutil installed or it's not part of the path.\nTo install gcutil see https://cloud.google.com/sdk/")
+        raise Exception("Failed to run 'gcloud version'. That means you don't have gcloud installed or it's not part of the path.\nTo install gcutil see https://cloud.google.com/sdk/\nPrevious versions of fabric_gce_tools used gcutil which is about to be deprecated.")
 
-    _gcutil_exists = (gcutil_version != None)
+    _gcloud_exists = (gcloud_version != None)
+
+def _get_data():
+    raw_data = subprocess.check_output("gcloud compute instances list --format=json", shell=True)
+    data = json.loads(raw_data)
+    return data
+
+def _get_roles(data):
+    roles = {}
+    for i in data:
+        if "tags" in i and i["tags"] and "items" in i["tags"]:
+            for t in i["tags"]["items"]:
+                role = t
+                if not role in roles:
+                    roles[role] = []
+
+                address = i["networkInterfaces"][0]["accessConfigs"][0]["natIP"]
+                if not address in roles[role]:
+                    roles[role].append(address)
+
+    return roles
 
 def update_roles_gce():
     """
@@ -50,32 +70,16 @@ def update_roles_gce():
     - On each function use the regular @roles decorator and set the role to the name
       of one of the tags associated with the instances you wish to work with
     """
-    global _gcutil_exists
+    global _gcloud_exists
 
-    if _gcutil_exists is None:
-        _check_gcutil()
+    if _gcloud_exists is None:
+        _check_gcloud()
     else:
-        if not _gcutil_exists:
-            raise Exception("Can't find 'gcutil'. That means you don't have gcutil installed or it's not part of the path.\nTo install gcutil see https://cloud.google.com/sdk/")
+        if not _gcloud_exists:
+            raise Exception("Can't find 'gcloud'. That means you don't have gcutil installed or it's not part of the path.\nTo install gcutil see https://cloud.google.com/sdk/")
 
-    roles = {}
-
-    raw_data = subprocess.check_output("gcutil listinstances --format=json", shell=True)
-    data = json.loads(raw_data)
-
-    for z in data["items"]:
-        if "instances" in data["items"][z]:
-            for i in data["items"][z]["instances"]:
-                if "tags" in i and i["tags"] and "items" in i["tags"]:
-                    for t in i["tags"]["items"]:
-                        role = t
-                        if not role in roles:
-                            roles[role] = []
-
-                        address = i["networkInterfaces"][0]["accessConfigs"][0]["natIP"]
-                        if not address in roles[role]:
-                            roles[role].append(address)
-
+    data = _get_data()
+    roles = _get_roles(data)
     env.roledefs.update(roles)
 
 __all__ = ["update_roles_gce"]
